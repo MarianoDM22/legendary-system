34b7b6c733ab913d2ffec413a6d0be400557a8e9
juliantorre <marco.julian.torre@gmail.com>
2017-11-03 00:06:24 -0400

    metodo insertar en ClientesDAO

--- TP_Programa/DAOS/ClientesDAO.php
+++ TP_Programa/DAOS/ClientesDAO.php
@@ -2,12 +2,41 @@
 
 namespace DAOS;
 
+/*
+ *	CREATE TABLE clientes(
+ *	id_cliente int auto_increment not null,
+ *	apellido varchar(30),
+ *	domicilio varchar(30),
+ *	nombre varchar(30),
+ *	telefono varchar(30),
+ *	primary key(id_cliente)
+ *	);
+ */
+
 class ClientesDAO extends SingletonAbstractDAO implements IDAO
 {
 	private $table = 'clientes';
 
 	public function insertar($dato){
+		$query = 'INSERT INTO '.$this->table.' 
+		( apellido , domicilio , nombre , telefono ) 
+		VALUES 
+		( :apellido , :domicilio , :nombre , :telefono )';
 
+		$pdo = new Connection();
+		$connection = $pdo->Connect();
+		$command = $connection->prepare($query);
+
+		$apellido = $dato->getApellido();
+		$domicilio = $dato->getDomicilio();
+		$nombre = $dato->getNombre();
+		$telefono = $dato->getTelefono();
+
+		$command->bindParam(':apellido', $apellido);
+		$command->bindParam(':domicilio', $domicilio);
+		$command->bindParam(':nombre', $nombre);
+		$command->bindParam(':telefono', $telefono);
+		$command->execute();
 	}
 	public function insertarDevolverID($dato){
 
