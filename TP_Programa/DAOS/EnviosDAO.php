64c26f18d67cc021fc199c0ea5b6e815e79b1c88
juliantorre <marco.julian.torre@gmail.com>
2017-11-03 00:30:23 -0400

    metodo insertarDevolverID en EnviosDAO

--- TP_Programa/DAOS/EnviosDAO.php
+++ TP_Programa/DAOS/EnviosDAO.php
@@ -2,6 +2,19 @@
 
 namespace DAOS;
 
+/*
+ *	CREATE TABLE envios(
+ *	id_envio int auto_increment not null,
+ *	domicilio varchar(100),
+ *	email varchar(100),
+ *	fecha_programada varchar(10),
+ *	hora_desde varchar(5),
+ *	hora_hasta varchar(5),
+ *	telefono varchar(30),
+ *	primary key(id_envio)
+ *	);
+ */
+
 class EnviosDAO extends SingletonAbstractDAO implements IDAO
 {
 	private $table = 'envios';
@@ -10,7 +23,33 @@ class EnviosDAO extends SingletonAbstractDAO implements IDAO
 
 	}
 	public function insertarDevolverID($dato){
+		$query = 'INSERT INTO '.$this->table.' 
+		( domicilio , email , fecha_programada , hora_desde , hora_hasta , telefono ) 
+		VALUES 
+		( :domicilio , :email , :fecha_programada , :hora_desde , :hora_hasta , :telefono )';
 
+		$pdo = new Connection();
+		$connection = $pdo->Connect();
+		$command = $connection->prepare($query);
+
+		$domicilio = $dato->getDomicilio();
+		$email = $dato->getEmail();
+		$fecha_programada = $dato->getFechaProgramada();
+		$hora_desde = $dato->getHoraDesde();
+		$hora_hasta = $dato->getHoraHasta();
+		$telefono = $dato->getTelefono();
+
+		$command->bindParam(':domicilio', $domicilio);
+		$command->bindParam(':email', $email);
+		$command->bindParam(':fecha_programada', $fecha_programada);
+		$command->bindParam(':hora_desde', $hora_desde);
+		$command->bindParam(':hora_hasta', $hora_hasta);
+		$command->bindParam(':telefono', $telefono);
+		$command->execute();
+
+		$dato->setId($connection->lastInsertId());
+			
+		return $dato;
 	}
 	public function buscarPorNombre($dato){
 
