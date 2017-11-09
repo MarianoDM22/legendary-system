c3e83f9a66fd63ac4c68876931f84ce06b95f904
juliantorre <marco.julian.torre@gmail.com>
2017-11-08 19:06:40 -0500

    Metodo insertarDevolverID en CuentasDAO

--- TP_Programa/DAOS/CuentasDAO.php
+++ TP_Programa/DAOS/CuentasDAO.php
@@ -2,6 +2,19 @@
 
 namespace DAOS;
 
+/*
+ *	CREATE TABLE cuentas(
+ *	id_cuenta int auto_increment not null,
+ *	email varchar(100),
+ *	pass varchar(100),
+ *	rol varchar(10),
+ *	fk_cliente int,
+ *	primary key(id_cuenta),
+ *	foreign key(fk_cliente) references clientes(id_cliente),
+ *	constraint unq_cuenta_email unique (email)
+ *	);
+ */
+
 class CuentasDAO extends SingletonAbstractDAO implements IDAO
 {
 	private $table = 'cuentas';
