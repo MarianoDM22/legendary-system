e7516d17dc9d982363edb24dd81341d8207af889
juliantorre <marco.julian.torre@gmail.com>
2017-11-08 19:13:11 -0500

    Metodo buscarPorNombre (email) en CuentasDAO

--- TP_Programa/DAOS/CuentasDAO.php
+++ TP_Programa/DAOS/CuentasDAO.php
@@ -66,7 +66,30 @@ class CuentasDAO extends SingletonAbstractDAO implements IDAO
 		return $dato;
 	}
 	public function buscarPorNombre($dato){
+		$object = null;
 
+		$query = 'SELECT * FROM '.$this->table.' WHERE email = :email';
+
+		$pdo = new Connection();
+		$connection = $pdo->Connect();
+		$command = $connection->prepare($query);			
+
+		$command->bindParam(':email', $dato);
+		$command->execute();
+
+		while ($row = $command->fetch())
+		{
+			$email = ($row['email']);
+			$pass = ($row['pass']);
+			$rol = ($row['rol']);
+			$fk_cliente = ($row['fk_cliente']);
+
+			$object = new \Modelos\Producto($email,$pass,$rol,$fk_cliente);
+
+			$object->setId($row['id_cuenta']);	
+		}
+
+		return $object;
 	}
 	public function buscarPorID($dato){
 		$object = null;
