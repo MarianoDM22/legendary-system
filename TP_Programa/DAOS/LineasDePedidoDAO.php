a60bf1e6c08ad9b00a3cc216686751bf0f1da58b
juliantorre <marco.julian.torre@gmail.com>
2017-11-08 19:24:51 -0500

    Comenzando a trabajar en LineasDePedidoDAO

--- TP_Programa/DAOS/LineasDePedidoDAO.php
+++ TP_Programa/DAOS/LineasDePedidoDAO.php
@@ -1,7 +1,16 @@
 <?php 
 
 namespace DAOS;
-
+/*
+ *	CREATE TABLE lineasdepedido(
+ *	id_lineadepedido int auto_increment not null,
+ *	cantidad int,
+ *	importe int,
+ *	fk_producto int not null,
+ *	primary key(id_lineadepedido),
+ *	foreign key(fk_producto) references productos(id_producto)
+ *	);
+ */ 
 class LineasDePedidoDAO extends SingletonAbstractDAO implements IDAO
 {
 	private $table = 'lineasdepedido';
