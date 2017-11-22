3157209e70013ee39078c700c6ba7368cce701c3
Julian Torre <marco.julian.torre@gmail.com>
2017-11-22 18:29:16 -0300

    Excepciones en CuentasDAO

--- TP_Programa/DAOS/CuentasDAO.php
+++ TP_Programa/DAOS/CuentasDAO.php
@@ -20,142 +20,186 @@ class CuentasDAO extends SingletonAbstractDAO implements IDAO
 	private $table = 'clientes';
 	private $table2 = 'cuentas';
 
-
-
-
 	public function insertar($dato)//le llega un objeto cliente y lo guarda en la base de datos
 	{
+		try 
+    	{
+			$query = 'INSERT INTO '.$this->table.' 
+			(email, pass, rol,fk_cliente) 
+			VALUES 
+			(:email, :pass, :rol,fk_cliente)';
 
-		$query = 'INSERT INTO '.$this->table.' 
-		(email, pass, rol,fk_cliente) 
-		VALUES 
-		(:email, :pass, :rol,fk_cliente)';
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);
+			$email = $dato->getEmail();
+			$pass = $dato->getPassword();
+			$rol = $dato->getRol();
+			$fk_cliente = $dato->getImagen();
 
-		$email = $dato->getEmail();
-		$pass = $dato->getPassword();
-		$rol = $dato->getRol();
-		$fk_cliente = $dato->getImagen();
-
-		$command->bindParam(':email', $email);
-		$command->bindParam(':pass', $pass);
-		$command->bindParam(':rol', $rol);
-		$command->bindParam(':fk_cliente', $fk_cliente);
+			$command->bindParam(':email', $email);
+			$command->bindParam(':pass', $pass);
+			$command->bindParam(':rol', $rol);
+			$command->bindParam(':fk_cliente', $fk_cliente);
 
 
-		$command->execute();
+			$command->execute();
+
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
+
 	}
 
 	public function insertarCuenta($dato)//le llega un objeto cuenta y lo guarda en BD
 	{
+		try 
+    	{
+			$query = 'INSERT INTO '.$this->table2.' 
+			(email, pass, rol,fk_cliente) 
+			VALUES 
+			(:email, :pass, :rol,:fk_cliente)';
 
-		$query = 'INSERT INTO '.$this->table2.' 
-		(email, pass, rol,fk_cliente) 
-		VALUES 
-		(:email, :pass, :rol,:fk_cliente)';
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);
+			$email = $dato->getEmail();
+			$pass = $dato->getPassword();
+			$rol = $dato->getRol();
+			$fk_cliente = $dato->getMCliente();
 
-		$email = $dato->getEmail();
-		$pass = $dato->getPassword();
-		$rol = $dato->getRol();
-		$fk_cliente = $dato->getMCliente();
+			$command->bindParam(':email', $email);
+			$command->bindParam(':pass', $pass);
+			$command->bindParam(':rol', $rol);
+			$command->bindParam(':fk_cliente', $fk_cliente);
 
-		$command->bindParam(':email', $email);
-		$command->bindParam(':pass', $pass);
-		$command->bindParam(':rol', $rol);
-		$command->bindParam(':fk_cliente', $fk_cliente);
+			$command->execute();
 
-		$command->execute();
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
+
 	}
 
 	public function insertarDevolverID($dato)//le llega un cliente sin ID
 	{
-		$query = 'INSERT INTO '.$this->table.' 
-		( apellido, domicilio,nombre,telefono) 
-		VALUES 
-		( :apellido, :domicilio,:nombre,:telefono)';
+		try 
+    	{
+			$query = 'INSERT INTO '.$this->table.' 
+			( apellido, domicilio,nombre,telefono) 
+			VALUES 
+			( :apellido, :domicilio,:nombre,:telefono)';
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);
 
-		
-		$apellido = $dato->getApellido();
-		$domicilio = $dato->getDomicilio();
-		$nombre = $dato->getNombre();
-		$telefono = $dato->getTelefono();
-
-		$command->bindParam(':apellido', $apellido);
-		$command->bindParam(':domicilio', $domicilio);
-		$command->bindParam(':nombre', $nombre);
-		$command->bindParam(':telefono', $telefono);
-
-
-		$command->execute();
-		
-
-		$dato->setId($connection->lastInsertId());
 			
-		return $dato; //devuelve el cliente con ID
+			$apellido = $dato->getApellido();
+			$domicilio = $dato->getDomicilio();
+			$nombre = $dato->getNombre();
+			$telefono = $dato->getTelefono();
+
+			$command->bindParam(':apellido', $apellido);
+			$command->bindParam(':domicilio', $domicilio);
+			$command->bindParam(':nombre', $nombre);
+			$command->bindParam(':telefono', $telefono);
+
+
+			$command->execute();
+			
+
+			$dato->setId($connection->lastInsertId());
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
 
 	}
 	public function buscarPorNombre($dato)
 	{
-		$object = null;
+		try 
+    	{
+			$object = null;
 
-		$query = 'SELECT * FROM '.$this->table2.' WHERE email = :email';//busco en table2 qe corresponde a CUENTAS
+			$query = 'SELECT * FROM '.$this->table2.' WHERE email = :email';//busco en table2 qe corresponde a CUENTAS
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);			
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);			
 
-		$command->bindParam(':email', $dato);
+			$command->bindParam(':email', $dato);
 
-		$command->execute();
+			$command->execute();
 
-		while ($row = $command->fetch())
-		{
-			$email=($row['email']);
-			$pass=($row['pass']);
-			$rol=($row['rol']);
-			$id_cliente=($row['fk_cliente']);
+			while ($row = $command->fetch())
+			{
+				$email=($row['email']);
+				$pass=($row['pass']);
+				$rol=($row['rol']);
+				$id_cliente=($row['fk_cliente']);
 
-			$object = new \Modelos\Cuenta($email, $pass, $rol,$id_cliente);//tomo los datos de la cuenta buscada y creo un objeto
-			$object->setId($row['id_cuenta']);		
-		}
+				$object = new \Modelos\Cuenta($email, $pass, $rol,$id_cliente);//tomo los datos de la cuenta buscada y creo un objeto
+				$object->setId($row['id_cuenta']);		
+			}
 
-		return $object;//retorno el objeto o null si no lo encontro
+			return $object;//retorno el objeto o null si no lo encontro
+
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
 
 	}
 	public function buscarClientePorID($dato)
 	{
-		$object = null;
+		try 
+    	{
+			$object = null;
 
-		$query = 'SELECT * FROM '.$this->table.' WHERE id_cliente = :id';
+			$query = 'SELECT * FROM '.$this->table.' WHERE id_cliente = :id';
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);			
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);			
 
-		$command->bindParam(':id', $dato);
-		$command->execute();
+			$command->bindParam(':id', $dato);
+			$command->execute();
 
-		while ($row = $command->fetch())
-		{
-			$apellido = ($row['apellido']);
-			$domicilio = ($row['domicilio']);
-			$nombre = ($row['nombre']);
-			$telefono = ($row['telefono']);
+			while ($row = $command->fetch())
+			{
+				$apellido = ($row['apellido']);
+				$domicilio = ($row['domicilio']);
+				$nombre = ($row['nombre']);
+				$telefono = ($row['telefono']);
 
-			$object = new \Modelos\Cliente( $nombre , $apellido , $domicilio , $telefono ) ;
+				$object = new \Modelos\Cliente( $nombre , $apellido , $domicilio , $telefono ) ;
 
-			$object->setId($row['id_cliente']);	
+				$object->setId($row['id_cliente']);	
+
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
 		}
 
 		
@@ -180,33 +224,43 @@ class CuentasDAO extends SingletonAbstractDAO implements IDAO
 	}
 	public function actualizarDatos($dato)
 	{
-		$query= 'UPDATE '.$this->table.'
-				SET apellido = :apellido, 
-					domicilio = :domicilio,
-					nombre = :nombre,
-					telefono = :telefono
-					
-				WHERE id_cliente = :id';
+		try 
+    	{
+			$query= 'UPDATE '.$this->table.'
+					SET apellido = :apellido, 
+						domicilio = :domicilio,
+						nombre = :nombre,
+						telefono = :telefono
+						
+					WHERE id_cliente = :id';
 
-		$pdo = new Connection();
-		$connection = $pdo->Connect();
-		$command = $connection->prepare($query);
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);
 
-		$id = $dato->getId();
-		$apellido = $dato->getApellido();
-		$domicilio = $dato->getDomicilio();
-		$nombre = $dato->getNombre();
-		$telefono = $dato->getTelefono();		
+			$id = $dato->getId();
+			$apellido = $dato->getApellido();
+			$domicilio = $dato->getDomicilio();
+			$nombre = $dato->getNombre();
+			$telefono = $dato->getTelefono();		
 
 
-		$command->bindParam(':id', $id);
-		$command->bindParam(':apellido', $apellido);
-		$command->bindParam(':domicilio', $domicilio);
-		$command->bindParam(':nombre', $nombre);
-		$command->bindParam(':telefono', $telefono);
-		
+			$command->bindParam(':id', $id);
+			$command->bindParam(':apellido', $apellido);
+			$command->bindParam(':domicilio', $domicilio);
+			$command->bindParam(':nombre', $nombre);
+			$command->bindParam(':telefono', $telefono);
+			
 
-		$command->execute();
+			$command->execute();
+
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
 
 	}
 }
